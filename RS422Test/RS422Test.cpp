bcb68b62f3ae7cab1d0a27158f2d99c1154c4bb5
Ryan-Tran <ryanhungtran5@gmail.com>
2019-03-02 19:35:05 -0800

    added RS422 tests  for TPC as the transmitter and FC as the receiver

---
+++ RS422Test/RS422Test.cpp
@@ -0,0 +1,45 @@
+#include "mbed.h"
+#include "RFM69/RFM69.h"
+
+#define NODE_ID (11)
+#define NETWORK_ID (100)
+#define FREQUENCY (RF69_433MHZ)
+#define TRANSMIT_TO (255)
+#define IS_RFM69HCW
+#define ENCRYPT_KEY "sampleEncryptKey"
+#define BAUDRATE (115200)
+
+#define RADIO_INT (PC_0)
+#define SPI1_MOSI (PA_7)
+#define SPI1_MISO (PA_6)
+#define SPI1_SCLK (PA_5)
+#define SPI1_SSEL (PA_4)
+#define RADIO_RST (PC_1)
+
+#define UART_FC_RX (PC_7)
+#define UART_FC_TX (PC_6)
+
+#define UART_GPS_RX (PA_10)
+#define UART_GPS_TX (PA_9)
+
+#define UART_DEBUG_RX (PA_3)
+#define UART_DEBUG_TX (PA_2)
+
+#define FC_SWITCH (PB_13)
+//#define EC_SWITCH (PB_12)
+
+#define BATTERY_READ (PC2)
+
+int main(){
+    Serial pc(UART_DEBUG_TX, UART_DEBUG_RX);
+    pc.baud(BAUDRATE);
+    pc.printf("PC connected.\r\n");
+
+    Serial fc(UART_FC_TX, UART_FC_RX);
+    fc.baud(BAUDRATE);
+
+    while(true){
+        pc.printf("PC connected.\r\n");
+        fc.printf("RS422 connected.\r\n");
+    }
+}
