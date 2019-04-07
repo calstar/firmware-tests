323f919af4be2b1409cfacc30b456eb168f0007d
Jaren Mendelsohn <jaren.mendelsohn@gmail.com>
2019-04-07 16:02:50 -0700

    MPL I2C without library

--- tests/altimeter.h
+++ tests/altimeter.h
@@ -1,50 +1,72 @@
 #include "mbed.h"
 #include "lib/MPL3115A2/MPL3115A2.h"
 #include "util.h"
+#include <string>
+
+using namespace std;
 
 #define BAUDRATE 9600
 I2C i2c_sensors(I2C_SENSOR_SDA, I2C_SENSOR_SCL);
 Serial debug_talk(DEBUG_TX, DEBUG_RX);
 
-Altitude altitude;
-MPL3115A2 altimeter(&i2c_sensors, &debug_talk);
+//Altitude altitude;
+//MPL3115A2 alt(&i2c_sensors, &debug_talk);
+
+#define SLAVE_ADDRESS 0xC0
+
+void I2C_WriteVerify(uint8_t data) {
+    int result = i2c_sensors.write(data);
+    if (result != 1) {
+        debug_talk.printf("I2C write failed: 0x%x\n", data);
+    }
+}
+
+void I2C_RegWrite(uint8_t reg, uint8_t data) {
+    i2c_sensors.start();
+    I2C_WriteVerify(SLAVE_ADDRESS);
+    I2C_WriteVerify(reg);
+    I2C_WriteVerify(data);
+}
+
+int8_t I2C_RegRead(uint8_t reg) {
+    i2c_sensors.start();
+    I2C_WriteVerify(SLAVE_ADDRESS);
+    I2C_WriteVerify(reg);
+    i2c_sensors.start();
+    I2C_WriteVerify(SLAVE_ADDRESS | 0x01);
+    return i2c_sensors.read(0);
+}
 
 int run_test() {
-    led_color(0, 0, 1);
-    altimeter.init();
+    led_color(1, 0, 0);
     debug_talk.baud(BAUDRATE);
     debug_talk.printf("\nBegin altimeter test\n");
-    uint8_t whoami = altimeter.whoAmI();
+    I2C_RegWrite(0x26, 0xB8);
+    I2C_RegWrite(0x13, 0x07);
+    I2C_RegWrite(0x26, 0xB9);
+    uint8_t whoami = I2C_RegRead(0x0C);
     debug_talk.printf("Whoami: 0x%x\n", whoami);
     if (whoami == 0xC4) {
         debug_talk.printf("Successfully connected\n");
-        Altitude a;
-        Temperature t;
-        Pressure p;
-        altimeter.setOffsetAltitude(83);
-        altimeter.setOffsetTemperature(20);
-        altimeter.setOffsetPressure(-32);
         while (true) {
-
-
-        altimeter.readAltitude(&a);
-        altimeter.readTemperature(&t);
-        
-        altimeter.setModeStandby();
-        altimeter.setModeBarometer();
-        altimeter.setModeActive();
-        altimeter.readPressure(&p);
-        
-        debug_talk.printf("Altitude: %sft, Temp: %sºF, Pressure: %sPa\r\n", a.print(), t.print(), p.print());
-        debug_talk.printf("OFF_H: 0x%X, OFF_T: 0x%X, OFF_P: 0x%X\r\n", altimeter.offsetAltitude(), altimeter.offsetTemperature(), altimeter.offsetPressure());
-    
-        altimeter.setModeStandby();
-        altimeter.setModeAltimeter();
-        altimeter.setModeActive();
+            debug_talk.printf("\n");
+            uint8_t status = I2C_RegRead(0x00);
+            debug_talk.printf("Status: %d\n", status);
+            if (status & 0x08) {
+                int32_t p_msb = I2C_RegRead(0x01);
+                int32_t p_csb = I2C_RegRead(0x02);
+                int32_t p_lsb = I2C_RegRead(0x03);
+                int32_t t_msb = I2C_RegRead(0x04);
+                int32_t t_lsb = I2C_RegRead(0x05);
+                debug_talk.printf("Data: %d %d %d %d %d\n", p_msb, p_csb, p_lsb, t_msb, t_lsb);
+                //double altitude = ((p_msb << 24) + (p_csb << 16) + (p_lsb << 8)) / 65536;
+                float tempcsb = (p_lsb>>4) / 16.0;
+                float altitude = (float)((p_msb << 8) | p_csb) + tempcsb;
+                debug_talk.printf("Altitude: %f\n", altitude);
+            }
+            wait(1);
         }
     } else {
-        while (true) {
-            debug_talk.printf("Failed to connect to altimeter, whoami: 0x%x\n", whoami);
-        }
+        debug_talk.printf("Failed to connect to altimeter, whoami: 0x%x\n", whoami);
     }
 }
