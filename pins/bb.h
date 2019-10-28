05daa2c93053f77732ad3f1d8c9a0bcf99383b33
Zitao Fang <zitaofang@berkeley.edu>
2019-10-28 01:53:47 +0000

    Checking bb

--- pins/bb.h
+++ pins/bb.h
@@ -1,10 +1,10 @@
 #define I2C_SENSOR_SDA PB_7
 #define I2C_SENSOR_SCL PB_6
 
-#define KX222_MOSI PA_7
-#define KX222_MISO PA_6
-#define KX222_SCLK PA_5
-#define KX222_CS   PB_6
+#define KX222_MOSI PB_15
+#define KX222_MISO PB_14
+#define KX222_SCLK PB_13
+#define KX222_CS   PB_12
 
 #define STATE_LED_RED PC_8
 #define STATE_LED_GREEN PA_8
--- tests/kx222.h
+++ tests/kx222.h
@@ -2,12 +2,13 @@
 #include "util.h"
 
 Serial debug_uart(DEBUG_TX, DEBUG_RX, 9600);
-SPI kx222_spi(KX222_MOSI, KX222_MISO, KX222_SCLK, KX222_CS);
+SPI kx222_spi(KX222_MOSI, KX222_MISO, KX222_SCLK);
+DigitalOut kx222_cs(KX222_CS);
 
 uint8_t regread(SPI *spi, uint8_t reg){
   uint8_t out;
-  spi->write(reg | 0x80);
-  out = spi->write(0x00);
+  spi->write((reg | 0x80));
+  out = spi->write((0x00));
 
   return out;
 }
@@ -22,18 +23,18 @@ int run_test() {
   uint8_t kxwhoami;
 
   debug_uart.printf("==========KX222 Test===========\r\n");
-  kx222_spi.format(8,0);
-  kx222_spi.frequency(100000);
+  kx222_spi.format(8,1);
+  kx222_spi.frequency(100000 / 4);
 
-  kx222_spi.select();
+  kx222_cs = 0;
   regwrite(&kx222_spi, 0x18, 0x80);
-  kx222_spi.deselect();
+  kx222_cs = 1;
   kx222_spi.write(0x0);
   while(1) {
-    kx222_spi.select();
+    kx222_cs = 0;
     kxwhoami = regread(&kx222_spi, 0x0F);
+    kx222_cs = 1;
     debug_uart.printf("kx222 whoami: %d\r\n", kxwhoami);
-    kx222_spi.deselect();
     kx222_spi.write(0x0);
   }
 
